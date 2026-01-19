6e9f436119494094f7e75d7ee6fd746da9d82332
Vitor <vitormv725@gmail.com>
2026-01-19 10:48:20 -0300

    Criando um buscador de arquivo txt

---
+++ TreinoJava/EscolherArquivo.java
@@ -0,0 +1,21 @@
+package Aprendizado.TreinoJava;
+import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
+import java.io.File;
+public class EscolherArquivo {
+      public static String caminho() {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileSelectionMode(
+                    JFileChooser.FILES_ONLY);
+            int result = fileChooser.showOpenDialog(null);
+            if (result == JFileChooser.CANCEL_OPTION)
+                return null;
+            File arquivo = fileChooser.getSelectedFile();
+            if(arquivo == null || arquivo.getName().equals(""))
+                JOptionPane.showMessageDialog(null, "Nome do arquivo inválido");
+            else
+                return arquivo.getPath();
+            return null;
+        }
+    }
+
---
+++ TreinoJava/FindFile.java
@@ -0,0 +1,39 @@
+package Aprendizado.TreinoJava;
+import javax.swing.JOptionPane;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class FindFile {
+    private  static RandomAccessFile arquivo;
+    public static void main(String[] args) {
+        try {
+            int codCli;
+            String nome, tel, email;
+            String caminho = EscolherArquivo.caminho();
+            arquivo = new RandomAccessFile(new File(caminho), "rw");
+            long a = arquivo.length();
+            arquivo.seek(a);
+            codCli = (int)(a / 100 + 1);
+            gravar(codCli);
+            nome = JOptionPane.showInputDialog("Digite o nome");
+            gravar(nome.toUpperCase(), 20);
+            tel = JOptionPane.showInputDialog("Digite o telefone");
+            gravar(tel.toUpperCase(), 8);
+            email = JOptionPane.showInputDialog("Digite o email");
+            gravar(email.toUpperCase(), 20);
+            arquivo.close();
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(null, "Erro no arquivo");
+        }
+    }
+    public static void gravar (String dado, int tamanho)
+        throws IOException{
+        StringBuffer buffer = new StringBuffer(dado);
+        buffer.setLength(tamanho);
+        arquivo.writeChars(buffer.toString());
+    }
+    public static void gravar (int codCli) throws IOException{
+        arquivo.writeInt(codCli);
+    }
+}
