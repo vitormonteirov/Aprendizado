fc1c18db0b09b273a77ba10d03c2c71a51a6ec34
Vitor Monteiro <vitormv725@gmail.com>
2026-01-28 08:34:08 -0300

    Criando uma GUI

--- UseTheHeadJAVA/testeJFrame.java
+++ UseTheHeadJAVA/testeJFrame.java
@@ -1,4 +1,19 @@
 package Aprendizado.UseTheHeadJAVA;
+import javax.swing.*;
+
+import static java.awt.Color.pink;
 
 public class testeJFrame {
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Eu te amo Sabrina");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+
+    }
 }
