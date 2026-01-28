c98c04fe5278553ac554661baabe1e85c65e967e
Vitor Monteiro <vitormv725@gmail.com>
2026-01-28 09:33:29 -0300

    Não consegui adicionar a imagem, algo deu errado

--- UseTheHeadJAVA/testeJFrame.java
+++ UseTheHeadJAVA/testeJFrame.java
@@ -1,19 +1,33 @@
 package Aprendizado.UseTheHeadJAVA;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import static java.awt.Color.pink;
+public class testeJFrame implements ActionListener {
+    JButton button;
 
-public class testeJFrame {
     public static void main(String[] args) {
+        testeJFrame gui = new testeJFrame();
+        gui.go();
+    }
 
-
+    public void go() {
         JFrame frame = new JFrame();
         JButton button = new JButton("Eu te amo Sabrina");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button.addActionListener(this);
+        this.button = button;
 
         frame.getContentPane().add(button);
-        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
         frame.setVisible(true);
+        }
 
+        public void actionPerformed(ActionEvent e) {
+         button.setText("Não me cutuque");
+    }
+    public void imagem(Graphics g){
+       Image image = new ImageIcon(getClass().getResource("/Aprendizado.png")).getImage();
     }
 }
