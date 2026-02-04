1993bc450c10d33a8461e6268765d2dee3fa1d49
Vitor Monteiro <vitormv725@gmail.com>
2026-02-04 08:19:11 -0300

    novas alteraçoes

--- UseTheHeadJAVA/testeJFrame.java
+++ UseTheHeadJAVA/testeJFrame.java
@@ -2,6 +2,7 @@ package Aprendizado.UseTheHeadJAVA;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class testeJFrame implements ActionListener {
     JButton button;
@@ -27,7 +28,27 @@ public class testeJFrame implements ActionListener {
         public void actionPerformed(ActionEvent e) {
          button.setText("Não me cutuque");
     }
+   //Não consegui inserir uma imagem
     public void imagem(Graphics g){
        Image image = new ImageIcon(getClass().getResource("/Aprendizado.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+
+        Random rand = new Random();
+        int red = rand.nextInt(256);
+        int green = rand.nextInt(256);
+        int blue = rand.nextInt(256);
+        Color startColor = new Color(red,green,blue);
+
+        red = rand.nextInt(255);
+        green = rand.nextInt(255);
+        blue = rand.nextInt(255);
+        Color endColor = new Color(red,green,blue);
+
+        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 150, 150);
     }
 }
