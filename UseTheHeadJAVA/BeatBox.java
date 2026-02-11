5940bfd9b8cc1c42de4c36fb5607feae80b38874
Vitor Monteiro <vitormv725@gmail.com>
2026-02-11 08:01:41 -0300

    Terminando codigo do beatbox e adicionando comentarios

--- UseTheHeadJAVA/BeatBox.java
+++ UseTheHeadJAVA/BeatBox.java
@@ -7,15 +7,15 @@ import java.util.ArrayList;
 import static javax.sound.midi.ShortMessage.*;
 
 public class BeatBox {
-    private ArrayList<JCheckBox> checkBoxList;
+    private ArrayList<JCheckBox> checkBoxList;               //Armazena caixas de Seleçao
     private Sequencer sequencer;
-    private Sequencer sequence;
+    private Sequence sequence;
     private Track track;
 
     String[] instrumentNames = { "Bass Drum", "Closed HiBat ", "Open Hibat", "Acoustic Snare", "Crash Cymball",
             "Hand Clap", "High Tom", "Bongo", "Maracas", "Whristle", "Low Conga", "Cowbell",
             "VibraSlap", "Low-mid tom", "High Agogo", "Open Hi Conga"};
-    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63}; //Teclas reais dos instrumentos
 
     public static void main(String[] args) {
         new BeatBox().buildGUI();
@@ -25,7 +25,7 @@ public class BeatBox {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
-        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); //Borda vazia nos fornece margem entre as bordas do painel
 
         Box buttonBox = new Box(BoxLayout.X_AXIS);
 
@@ -34,13 +34,13 @@ public class BeatBox {
         buttonBox.add(start);
 
         JButton stop = new JButton("Stop");
-        stop.addActionListener(e-> sequence.stop());
+        stop.addActionListener(e-> sequencer.stop());
         buttonBox.add(stop);
 
         JButton upTempo = new JButton("Up Tempo");
         upTempo.addActionListener(e -> changeTempo(1.03f));
         buttonBox.add(upTempo);
-
+// Ajuste de tempo por clique
         JButton downTempo = new JButton("Down Tempo");
         downTempo.addActionListener(e -> changeTempo(0.97f));
         buttonBox.add(downTempo);
@@ -48,7 +48,7 @@ public class BeatBox {
         Box nameBox = new Box(BoxLayout.Y_AXIS);
         for (String instrumentName : instrumentNames) {
             JLabel instrLabel = new JLabel(instrumentName);
-            instrLabel.setBorder(BorderFactory.createEmptyBorder(4, 1, 4, 1));
+            instrLabel.setBorder(BorderFactory.createEmptyBorder(4, 1, 4, 1)); //Alinha o instrumento com a caixa de seleçao
 
         }
         background.add(BorderLayout.EAST, buttonBox);
@@ -65,7 +65,7 @@ public class BeatBox {
 
         checkBoxList = new ArrayList<>();
         for (int i = 0; i < 256; i++) {
-            JCheckBox c = new JCheckBox();
+            JCheckBox c = new JCheckBox();  //Seleciona as caiuxas e as define como falsas para que não estejam marcadas
             c.setSelected(false);
             checkBoxList.add(c);
             mainPanel.add(c);
@@ -81,17 +81,17 @@ public class BeatBox {
         try {
             sequencer = MidiSystem.getSequencer();
             sequencer.open();
-            sequence = new Sequence(Sequence.PPQ, 4);
-            track = sequence.getSequence().createTrack();
+            sequence = new Sequence(Sequence.PPQ, 4); //configs padrões da MIDI
+            track = sequence.createTrack();
             sequencer.setTempoInBPM(120);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    private void buildTrackAndStart() {
-        int[] trackList;
+    private void buildTrackAndStart() { //Criar array de 16 elementos para armazenar os valores das batidas
+        int[] trackList;                //Se o instrumento nao tocar a batida, insere um zero
 
-        sequence.deleteTrack(Track);
+        sequence.deleteTrack(track); //Cancela antiga e insere nova
         track = sequence.createTrack();
 
         for (int i = 0; i < 16; i++) {
@@ -100,15 +100,52 @@ public class BeatBox {
             int key = instruments[i];
 
             for (int j = 0; j < 16; j++) {
-                JCheckBox c = checkBoxList.get(j + 16 * i);
+                JCheckBox jc = checkBoxList.get(j + 16 * i);
                 if (jc.isSelected()){
                     trackList[j] = key;
                 } else {
                     trackList[j] = 0;
                 }
             }
-
+            makeTracks(trackList); //Cria eventos a cada batida de instrumento
+            track.add(makeEvent(CONTROL_CHANGE, 1, 127, 0, 16));
         }
+        track.add(makeEvent(PROGRAM_CHANGE, 9, 1, 0, 15));
+
+        try {
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.setTempoInBPM(120);
+            sequencer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void changeTempo(float tempoMultiplier) {
+        float tempoFactor = sequencer.getTempoFactor();
+        sequencer.setTempoFactor(tempoFactor * tempoMultiplier);
+    }
+    private void makeTracks(int[] List) {
+        for (int i = 0; i < 16; i++) {
+            int key= List[i];
+
+            if (key != 0){
+                track.add(makeEvent(NOTE_ON, 9, key, 100, i));
+                track.add(makeEvent(NOTE_OFF, 9, key, 100, i + 1));
+            }
+        }
+    }
+    public static MidiEvent makeEvent(int cmd, int chn1, int one, int two, int tick){
+        MidiEvent event = null;
+        try {
+            ShortMessage msg = new ShortMessage();
+            msg.setMessage(cmd, chn1, one, two);
+            event = new MidiEvent(msg, tick);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return event;
     }
 
 }
