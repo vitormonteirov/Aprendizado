5bbc892865ba5f9eb80b3161da55e79b83eb2935
Vitor Monteiro <vitormv725@gmail.com>
2026-03-10 09:20:19 -0300

    Algumas atividades conjuntas a tentativa de introduçao de exceptions

--- BeeCrowd/VerIdade.java
+++ BeeCrowd/VerIdade.java
@@ -1,4 +1,18 @@
 package Aprendizado.BeeCrowd;
+import java.util.*;
 
 public class VerIdade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        if (idade > 0 && idade < 16) {
+            System.out.println("Ainda não pode votar!");
+        }
+        else {
+            System.out.println("Você já pode votar!");
+        }
+    }
 }
