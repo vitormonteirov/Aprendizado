f27b144e934e61ad44b7f2abaa0a5583230b8771
Vitor Monteiro <vitormv725@gmail.com>
2026-04-14 10:23:59 -0300

    teste

---
+++ BeeCrowd/soma2.java
@@ -0,0 +1,11 @@
+package Aprendizado.BeeCrowd;
+
+public class soma2 {
+    public static void main(String[] args) {
+        int X = 2;
+        int Y = 3;
+
+        int soma = X + Y;
+        System.out.println("A= " + soma);
+    }
+}
