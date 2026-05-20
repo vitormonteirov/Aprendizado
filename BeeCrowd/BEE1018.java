f7e239c349528b5fd0e35c389818e123ea0c7925
Vitor Monteiro <vitormv725@gmail.com>
2026-05-20 08:55:29 -0300

    2 atividades do BEECROWD

--- BeeCrowd/BEE1018.java
+++ BeeCrowd/BEE1018.java
@@ -1,4 +1,44 @@
 package Aprendizado.BeeCrowd;
+import java.util.Scanner;
 
 public class BEE1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int valor = sc.nextInt();
+
+        System.out.println(valor);
+
+        int restante = valor;
+
+        int notas100 = restante / 100;
+        restante %= 100;
+
+        int notas50 = restante / 50;
+        restante %= 50;
+
+        int notas20 = restante / 20;
+        restante %= 20;
+
+        int notas10 = restante / 10;
+        restante %= 10;
+
+        int notas5 = restante / 5;
+        restante %= 5;
+
+        int notas2 = restante / 2;
+        restante %= 2;
+
+        int notas1 = restante;
+
+        System.out.println(notas100 + " nota(s) de R$ 100,00");
+        System.out.println(notas50 + " nota(s) de R$ 50,00");
+        System.out.println(notas20 + " nota(s) de R$ 20,00");
+        System.out.println(notas10 + " nota(s) de R$ 10,00");
+        System.out.println(notas5 + " nota(s) de R$ 5,00");
+        System.out.println(notas2 + " nota(s) de R$ 2,00");
+        System.out.println(notas1 + " nota(s) de R$ 1,00");
+
+        sc.close();
+    }
 }
--- BeeCrowd/BEE1019.java
+++ BeeCrowd/BEE1019.java
@@ -1,4 +1,17 @@
 package Aprendizado.BeeCrowd;
+import java.util.*;
 
 public class BEE1019 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N =  sc.nextInt();
+        int horas = N / 3600;
+        int minutos = (N % 3600) / 60;
+        int segundos = N % 60;
+
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+
+        sc.close();
+    }
 }
