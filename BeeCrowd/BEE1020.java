9911e4a3873a5affd277db782f04b3d4e5c331f9
Vitor Monteiro <vitormv725@gmail.com>
2026-07-08 17:28:13 -0300

    a

--- BeeCrowd/BEE1020.java
+++ BeeCrowd/BEE1020.java
@@ -1,4 +1,22 @@
 package Aprendizado.BeeCrowd;
+import java.util.*;
 
 public class BEE1020 {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int idadeEmDias = sc.nextInt();
+        int ano;
+        int mes;
+        int dia;
+
+        ano = idadeEmDias / 365;
+        mes = (idadeEmDias % 365) / 30;
+        dia = (idadeEmDias % 365) % 30;
+
+            System.out.println(ano + " ano(s)");
+            System.out.println(mes + " mes(es)");
+            System.out.println(dia + " dia(s)");
+            sc.close();
+        }
+    }
